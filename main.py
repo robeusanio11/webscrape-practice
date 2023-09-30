8ffa5c988c243ed1e4cea7c08d3755b4de2306d1
robeusanio11 <robeusanio11@gmail.com>
2023-09-30 13:49:31 -0700

    got player name from basketballreference

---
+++ main.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+html_text = requests.get('https://www.basketball-reference.com/teams/DEN/2024.html').text;
+soup = BeautifulSoup(html_text, 'lxml');
+player_cards = soup.find_all('td', attrs={"data-stat": "player"});
+firstPlayerName = player_cards[0].find('a').text;
+print(firstPlayerName);
