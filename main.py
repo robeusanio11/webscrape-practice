03cfca4234721fabf3777ab0fae4a7995fede88d
robeusanio11 <robeusanio11@gmail.com>
2023-09-30 23:51:45 -0700

    scraped for all teams/django setup

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.basketball-reference.com/teams/DEN/2024.html').text;
-soup = BeautifulSoup(html_text, 'lxml');
-player_cards = soup.find_all('td', attrs={"data-stat": "player"});
-firstPlayerName = player_cards[0].find('a').text;
+teams = ["ATL", "BOS", "BRK", "CHO", "CHI", "CLE", "DAL", "DEN", "DET", "GSW", "HOU", "IND", "LAC", "LAL", "MEM", "MIA", "MIL", "MIN", "NOH", "NYK", "OKC", "ORL", "PHI", "PHO", "POR", "SAC", "SAS", "TOR", "UTA", "WAS"]
+for team in teams:
+    html_text = requests.get(f"https://www.basketball-reference.com/teams/{team}/2024.html").text;
+    soup = BeautifulSoup(html_text, 'lxml');
+    player_cards = soup.find_all('td', attrs={"data-stat": "player"});
 
-for player in player_cards:
-    print(player.find('a').text);
+    print (team + " 2024 Roster:");
+    for player in player_cards:
+        print(player.find('a').text);
+
